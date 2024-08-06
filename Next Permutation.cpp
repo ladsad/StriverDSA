24d59e86f3b75db94f3e87e303820ac7f070c65a
ladsad <shaurya23july@gmail.com>
2024-08-06 22:20:55 +0530

    fix: fixed formatting

--- Next Permutation.cpp
+++ Next Permutation.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    public:
-  void nextPermutation(vector<int>& nums) {
-    const int n = nums.size();
-    int i;
-    for (i = n - 2; i >= 0; --i)
-      if (nums[i] < nums[i + 1])
-        break;
+    void nextPermutation(vector<int>& nums) {
+        const int n = nums.size();
+        int i;
+        for (i = n - 2; i >= 0; --i)
+        if (nums[i] < nums[i + 1])
+            break;
 
-    if (i >= 0)
-      for (int j = n - 1; j > i; --j)
-        if (nums[j] > nums[i]) {
-          swap(nums[i], nums[j]);
-          break;
-        }
+        if (i >= 0)
+        for (int j = n - 1; j > i; --j)
+            if (nums[j] > nums[i]) {
+            swap(nums[i], nums[j]);
+            break;
+            }
 
-    reverse(nums, i + 1, n - 1);
-  }
+        reverse(nums, i + 1, n - 1);
+    }
 
- private:
-  void reverse(vector<int>& nums, int l, int r) {
-    while (l < r)
-      swap(nums[l++], nums[r--]);
-  }
+private:
+    void reverse(vector<int>& nums, int l, int r) {
+        while (l < r)
+        swap(nums[l++], nums[r--]);
+    }
 };
